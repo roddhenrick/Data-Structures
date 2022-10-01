4bd6b5abbda6df3de279cb2ad89b865d8227c46a
roddhenrick369 <roddhenrick369@gmail.com>
2022-10-01 08:56:20 -0300

    add prepend method

--- linked_list.py
+++ linked_list.py
@@ -27,11 +27,15 @@ class LinkedList:
         self.length += 1
 
     def prepend(self, value):
-        ...
+        node = self._node(value)
+        node['next_node'] = self.head
+        self.head = node
+        self.length += 1
 
         
 
 my_linked_list = LinkedList(10)
 my_linked_list.append(30)
 my_linked_list.append(56)
+my_linked_list.prepend(1)
 print(my_linked_list)
