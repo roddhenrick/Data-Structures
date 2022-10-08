3d660c1d28d8b0f37d74109840f75501c025e168
roddhenrick369 <roddhenrick369@gmail.com>
2022-10-08 01:50:43 -0300

    refactor remove method

--- binary_tree.py
+++ binary_tree.py
@@ -61,69 +61,37 @@ class BinaryTree:
 
     def remove(self, value):
         node = self.root
-        branch_root  = self.root
+        parent  = self.root
+
+        if node is None:
+            return "Empty Tree"
 
         while True:
-            if node is None:
-                break
 
-            if value == node.value:
-                if branch_root.left and branch_root.left.value == value:
-                    branch = branch_root.left
-
-                if branch_root.right and branch_root.right.value == value:
-                    branch = branch_root.right
-
-                subtree = branch.left
-                rightmost_subtree = subtree.right
-                previous = rightmost_subtree
-
-                while rightmost_subtree is not None:
-                    previous = rightmost_subtree
-                    rightmost_subtree = rightmost_subtree.right
-
-                rightmost_subtree = previous
-                
-                while True:
-                    
-                    if subtree is not None:
-                        rightmost_subtree.right = branch.right
-                        if branch == branch_root.right:
-                            branch_root.right = subtree
-                        else:
-                            branch_root.left = subtree
-                        break
-                    else:
-                        if branch == branch_root.right:
-                            branch_root.right = None
-                        else:
-                            branch_root.left = None
-                        break
-                break
-
-            elif value < node.value:
-                branch_root = node
+            if value < node.value:
+                parent = node
                 node = node.left
             elif value > node.value:
-                branch_root = node
+                parent = node
                 node = node.right
+            elif node.value == value:
+                if node.left == None and  node.right == None:
+                    if parent.left.value == value:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                    return
+                        
 
 
 
 bi_tree = BinaryTree()
-bi_tree.insert(60)
+bi_tree.insert(50)
 bi_tree.insert(30)
-bi_tree.insert(51)
-bi_tree.insert(38)
-bi_tree.insert(55)
-bi_tree.insert(44)
-bi_tree.insert(54)
-bi_tree.insert(72)
-bi_tree.insert(73)
-bi_tree.insert(78)
-bi_tree.insert(85)
-bi_tree.insert(86)
-bi_tree.insert(90)
-bi_tree.insert(99)
-bi_tree.remove(51)
+bi_tree.insert(20)
+bi_tree.insert(40)
+bi_tree.insert(70)
+bi_tree.insert(80)
+bi_tree.insert(60)
+bi_tree.remove(80)
 print(bi_tree)
